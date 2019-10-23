593615fb25408bee8859d81751ad5d451fc847ea
Alexandr Glebov <aglebov.dev@gmail.com>
2019-10-23 13:11:59 +0300

    Copy commit

---
+++ src/FOMOD.Creator/Converters/EnumValueToDescriptionConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace FOMOD.Creator.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public sealed class EnumValueToDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+            var type = value.GetType();
+            if (!type.IsEnum && type != typeof(bool))
+                return null;
+            var field = type == typeof(bool) ? value.ToString() : type.GetField(value.ToString()).Name;
+            return Localize.JsonLocalizeProvider.Default.GetLocalizedObject($"{type.Name}-{field}");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ src/FOMOD.Creator/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,59 @@
+﻿namespace FOMOD.Creator.ViewModels
+{
+    using FOMOD.Creator.Interfaces;
+    using FOMOD.Creator.PrismEvent;
+    using MahApps.Metro.Controls.Dialogs;
+    using Prism.Events;
+    using Prism.Regions;
+    using PropertyChanged;
+    using StructureMap;
+    using System.Globalization;
+    using WPFLocalizeExtension.Engine;
+
+    [AddINotifyPropertyChangedInterface]
+    public class WelcomeViewModel : ProjectWorkerBaseViewModel, IHaveDisplayName
+    {
+        public System.Collections.ObjectModel.ObservableCollection<System.Globalization.CultureInfo> Cultures { get; set; }
+        public CultureInfo SelectedCulture { get; set; }
+
+        void OnSelectedCultureChanged()
+        {
+            LocalizeDictionary.Instance.SetCurrentThreadCulture = true;
+            LocalizeDictionary.Instance.Culture = SelectedCulture;
+            SaveSettings();
+        }
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.Culture = SelectedCulture?.Name;
+            Properties.Settings.Default.Save();
+        }
+
+        public WelcomeViewModel(IEventAggregator eventAggregator, IDialogCoordinator dialogCoordinator, IContainer container, IRegionManager regionManager)
+            : base(dialogCoordinator, container, eventAggregator, regionManager)
+        {
+            eventAggregator.GetEvent<OpenLink>().Subscribe(OpenProject);
+
+            Cultures = new System.Collections.ObjectModel.ObservableCollection<System.Globalization.CultureInfo>(Localize.JsonLocalizeProvider.Default.GetCultures());
+            var curent = Properties.Settings.Default.Culture;
+            if (string.IsNullOrWhiteSpace(curent))
+            {
+                SelectedCulture = CultureInfo.InvariantCulture;
+            }
+            else
+            {
+                SelectedCulture = CultureInfo.GetCultureInfo(curent);
+            }
+
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                return Localize.JsonLocalizeProvider.Default
+                    .GetLocalizedObject("app-main-tab-name", null, WPFLocalizeExtension.Engine.LocalizeDictionary.Instance.Culture)
+                    .ToString();
+            }
+        }
+    }
+}
